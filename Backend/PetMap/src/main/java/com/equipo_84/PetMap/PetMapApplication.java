6dfa12f5f5999dc3c5766af8e607e6ed0f9a8a7a
Ulises Rosales <ulises223366@gmail.com>
2025-01-17 16:33:09 -0300

    Estructura inicial
    Estructura inicial del back-end

---
+++ Backend/PetMap/src/main/java/com/equipo_84/PetMap/PetMapApplication.java
@@ -0,0 +1,13 @@
+package com.equipo_84.PetMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMapApplication.class, args);
+	}
+
+}
---
+++ Backend/PetMap/src/test/java/com/equipo_84/PetMap/PetMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo_84.PetMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
