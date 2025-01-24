5a279dc89394849a24ccbcdc30582d8f446741bb
Nelson Musto <mustonelson@gmail.com>
2025-01-24 17:06:31 -0300

    Implementación: endpoint para buscar mascota por id + validación de seguridad en entidad Mascota

--- Backend/PetMap/src/main/java/com/equipo_84/PetMap/service/mascotaService/MascotaService.java
+++ Backend/PetMap/src/main/java/com/equipo_84/PetMap/service/mascotaService/MascotaService.java
@@ -1,10 +1,13 @@
 package com.equipo_84.PetMap.service.mascotaService;
 
-import com.equipo_84.PetMap.dto.MascotaDTO;
 import com.equipo_84.PetMap.entity.Mascota;
 import com.equipo_84.PetMap.repository.IMascotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class MascotaService implements IMascotaService{
@@ -14,16 +17,14 @@ public class MascotaService implements IMascotaService{
 
 
     @Override
-    public Mascota save(MascotaDTO mascotaDTO) {
-        Mascota mascota = new Mascota();
-
-        mascota.setNombre(mascotaDTO.getNombre());
-        mascota.setEspecie(mascotaDTO.getEspecie());
-        mascota.setRaza(mascotaDTO.getRaza());
-        mascota.setDescripcion(mascotaDTO.getDescripcion());
-        mascota.setUrlFoto(mascotaDTO.getUrlFoto());
-
+    public Mascota save(Mascota mascota) {
         mascota = mascotaRepository.save(mascota);
         return mascota;
+    }
+
+    @Override
+    public Mascota findMascotaById(Long id) {
+        return mascotaRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Mascota con ID " + id + " no encontrada"));
     }
 }
